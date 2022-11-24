f993c77ef84ad456d25daddb58a2c36756abedb7
wang-yurin <king_1994@naver.com>
2022-11-24 17:53:28 +0900

    feat(request): 데이터 받아오는 함수 구현
    fetch API 사용

---
+++ src/api/request.js
@@ -0,0 +1,13 @@
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default request;
